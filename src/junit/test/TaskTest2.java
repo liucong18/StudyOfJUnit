69fbf4a352419525d41ee31e2f3a3e92a686caf1
iucong18 <718588047@qq.com>
2019-02-11 22:37:58 +0800

    测试套件

---
+++ src/junit/test/TaskTest2.java
@@ -0,0 +1,12 @@
+package junit.test;
+
+import org.junit.Test;
+
+public class TaskTest2 {
+
+	@Test
+	public void test() {
+		System.out.println("this is TaskTest2...");
+	}
+
+}
---
+++ src/junit/test/TaskTest3.java
@@ -0,0 +1,12 @@
+package junit.test;
+
+import org.junit.Test;
+
+public class TaskTest3 {
+
+	@Test
+	public void test() {
+		System.out.println("this is TaskTest3...");
+	}
+
+}
